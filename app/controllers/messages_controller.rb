68c1b7b0821535bf2daf583c0e511b15f2b917fa
keigod77 <keigod0707@gmail.com>
2019-08-02 13:18:25 +0900

    create-MessageModel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
+    @message = Message.new
+    @messages = 
   end
+
+  def create
+  end
+
+  private
+  
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
 end
